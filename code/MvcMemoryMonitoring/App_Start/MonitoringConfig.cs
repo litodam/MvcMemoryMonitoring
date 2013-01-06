797d15d9036226aacb756db7b11eb1bc89cdaba9
litodam <pablo.damiani@southworks.net>
2013-01-06 17:03:08 -0300

    initial solution, perf counters page in place, memory still WIP

---
+++ code/MvcMemoryMonitoring/App_Start/MonitoringConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace MvcMemoryMonitoring.App_Start
+{
+    using System;
+    using System.Configuration;
+
+    public class MonitoringConfig
+    {
+        public static void Register()
+        {
+            bool appDomainMonitoringEnabled;
+            bool.TryParse(ConfigurationManager.AppSettings["AppDomainMonitoringEnabled"], out appDomainMonitoringEnabled);
+            
+            if (appDomainMonitoringEnabled)
+            {
+                AppDomain.MonitoringIsEnabled = true;
+            }
+        }
+    }
+}
---
+++ code/MvcMemoryMonitoring/Controllers/MonitorController.cs
@@ -0,0 +1,17 @@
+﻿namespace MvcMemoryMonitoring.Controllers
+{
+    using System.Web.Mvc;
+
+    public class MonitorController : Controller
+    {
+        public ActionResult Memory()
+        {
+            return View();
+        }
+
+        public ActionResult PerfCounters()
+        {
+            return View();
+        }
+    }
+}
