abcca35f245991f5b76a79f82506a2c21e99fad5
litodam <pablo.damiani@southworks.net>
2013-01-06 23:23:05 -0300

    added memory info w/perf counters view, also an outputcache view showing the # of outputcache entries

--- code/MvcMemoryMonitoring/Controllers/MonitorController.cs
+++ code/MvcMemoryMonitoring/Controllers/MonitorController.cs
@@ -1,15 +1,24 @@
 ﻿namespace MvcMemoryMonitoring.Controllers
 {
+    using System.Web;
     using System.Web.Mvc;
 
     public class MonitorController : Controller
     {
         public ActionResult Memory()
-        {
+        {            
+            var res = HttpRuntime.Cache.EffectivePrivateBytesLimit;
+            
             return View();
         }
 
+        [OutputCache(Duration = 3000, VaryByParam="*")]
         public ActionResult PerfCounters()
+        {
+            return View();
+        }
+        
+        public ActionResult OutputCache()
         {
             return View();
         }
